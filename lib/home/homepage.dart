c01cf83d8f4721ccf505875d70c290678a76688c
Nodirbek Rasuljonov <nodirbekmaqsudjonovich@gmail.com>
2024-02-23 19:35:46 +0100

    some changes in homepage added bacgroundmage and etc

--- lib/home/homepage.dart
+++ lib/home/homepage.dart
@@ -6,7 +6,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Page"),),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Home Page",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24.0,
+          ),
+          
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage("assets/images/homebg.jpg"),
+          ),
+        ),
+      ),
     );
   }
 }
