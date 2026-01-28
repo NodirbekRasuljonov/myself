c45a4b794a092ddf51d37a194e3661d5a4b2ce2d
Nodirbek Rasuljonov <nodirbekrasuljonov@Nodirbeks-MacBook-Air.local>
2026-01-28 23:43:43 +0100

    added autoopen for odf

--- lib/home/homepage.dart
+++ lib/home/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myself/const/color_const.dart';
 import 'package:myself/const/text_const.dart';
+import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/services.dart';
 import 'dart:io';
@@ -94,4 +95,5 @@ Future<void> saveCV() async {
     byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes),
   );
   debugPrint("File saved to ${file.path}");
+  await OpenFile.open(file.path);
 }
